1316fec64d5fa2203c0347a8b9cf8ce12291e2c1
Stanislav <grafwwwolf@yandex.ru>
2022-07-04 19:38:04 +0300

    HW9

--- src/main/java/ru/pigarev/market/controllers/ProductsController.java
+++ src/main/java/ru/pigarev/market/controllers/ProductsController.java
@@ -2,8 +2,10 @@ package ru.pigarev.market.controllers;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import ru.pigarev.market.dto.ProductDto;
+import ru.pigarev.market.exceptions.ResourceNotFoundException;
 import ru.pigarev.market.model.Product;
 import ru.pigarev.market.services.ProductService;
 
@@ -11,6 +13,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
+@RequestMapping("/api/v1")
 @RestController
 public class ProductsController {
 
@@ -24,32 +27,35 @@ public class ProductsController {
         return productService.findAll(pageIndex - 1, 10).map(ProductDto::new);
     }
 
-    @GetMapping("/products/after/{minPrice}")
-    public List<ProductDto> findByCostAfter(@PathVariable Double minPrice) {
-
-        return productService.findByCostAfter(minPrice).stream().map(ProductDto::new).collect(Collectors.toList());
-    }
-
-    @GetMapping("/products/before/{maxPrice}")
-    public List<ProductDto> findByCostBefore(@PathVariable Double maxPrice) {
-
-        return productService.findByCostBefore(maxPrice).stream().map(ProductDto::new).collect(Collectors.toList());
-    }
-
-    @GetMapping("/products/between")
-    public List<ProductDto> findByCostBetween(@RequestParam(name = "min") Double minPrice,
-                                              @RequestParam(name = "max") Double maxPrice) {
-
-        return productService.findByCostBetween(minPrice, maxPrice).stream().map(ProductDto::new).collect(Collectors.toList());
-    }
+//    @GetMapping("/products/after/{minPrice}")
+//    public List<ProductDto> findByCostAfter(@PathVariable Double minPrice) {
+//
+//        return productService.findByCostAfter(minPrice).stream().map(ProductDto::new).collect(Collectors.toList());
+//    }
+//
+//    @GetMapping("/products/before/{maxPrice}")
+//    public List<ProductDto> findByCostBefore(@PathVariable Double maxPrice) {
+//
+//        return productService.findByCostBefore(maxPrice).stream().map(ProductDto::new).collect(Collectors.toList());
+//    }
+//
+//    @GetMapping("/products/between")
+//    public List<ProductDto> findByCostBetween(@RequestParam(name = "min") Double minPrice,
+//                                              @RequestParam(name = "max") Double maxPrice) {
+//
+//        return productService.findByCostBetween(minPrice, maxPrice).stream().map(ProductDto::new).collect(Collectors.toList());
+//    }
 
     @GetMapping("/products/{id}")
     public ProductDto findById(@PathVariable Long id) {
 
-        return new ProductDto(productService.findById(id).get());
+        return new ProductDto(productService
+                .findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Product id = " + id + " not found")));
     }
 
     @PostMapping("/products")
+    @ResponseStatus(HttpStatus.CREATED)
     public ProductDto save(@RequestBody ProductDto productDto) {
 
         Product product = new Product();
@@ -65,6 +71,5 @@ public class ProductsController {
     public void removeProduct(@PathVariable Long id) {
 
         productService.removeProduct(id);
-
     }
 }
--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -1,13 +1,13 @@
 angular.module('market-front', []).controller('appController', function ($scope, $http) {
 
-    const contextPath = 'http://localhost:8189/market/';
+    const contextPath = 'http://localhost:8189/market/api/v1';
     let currentPage = 1;
     let totalPgs;
 
     $scope.loadProducts = function (pageIndex = currentPage) {
 
         $http({
-            url: contextPath + 'products',
+            url: contextPath + '/products',
             method: 'GET',
             params: {
                 page: pageIndex
@@ -15,12 +15,13 @@ angular.module('market-front', []).controller('appController', function ($scope,
         }).then(function (response) {
             console.log(response);
             $scope.productsPage = response.data;
+            $scope.paginationArray = $scope.generatePagesIndexes(1, $scope.productsPage.totalPages);
             totalPgs = response.data.totalPages;
         });
     }
 
     $scope.removeProduct = function (product) {
-        $http.get(contextPath + 'products/delete/' + product.id)
+        $http.get(contextPath + '/products/delete/' + product.id)
             .then(function (response) {
                 console.log(response);
                 $scope.loadProducts();
@@ -47,6 +48,25 @@ angular.module('market-front', []).controller('appController', function ($scope,
             currentPage++;
             $scope.loadProducts();
         }
+    }
+
+    $scope.createNewProduct = function () {
+        $http.post(contextPath + '/products', $scope.new_product)
+            .then(function successCallback(response) {
+                    $scope.loadProducts(currentPage);
+                    $scope.new_product = null;
+                },
+                function failCallback(response) {
+                    alert(response.data.message);
+                });
+    }
+
+    $scope.generatePagesIndexes = function (startPage, endPage) {
+        let arr = [];
+        for (let i = startPage; i < endPage + 1; i++) {
+            arr.push(i);
+        }
+        return arr;
     }
 
     $scope.loadProducts(currentPage);
